cf60805bb6636017b9e3ea08aed34da3443e7c5b
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-07 15:06:20 +0800

    Update Poller.hpp

--- MyMuduo/include/Poller.hpp
+++ MyMuduo/include/Poller.hpp
@@ -8,32 +8,30 @@ class EventLoop;
 
 using ChannelList = vector<Channel *>;
 using ChannelMap = unordered_map<int, Channel *>;
-/*
-*  io复用模块 muduo中多路事件分发器的核心
-*/
+
+// 每一个EventLoop对应一个Poller
+// io复用模块 muduo中多路事件分发器的核心
+
 class Poller : NonCopyable
 {
 public:
     Poller(EventLoop *loop);
-
     virtual ~Poller() = default;
 
-    //所有io复用保留统一接口
+    // 监控事件，使用channel来具体操作
     virtual TimeStamp poll(int timeout, ChannelList *active_channels) = 0;
-
-    //判断channel是否在当前poller中
-    bool has_channel(Channel *channel) const;
-
-    //获取这个事件循环的poller
-    static Poller *new_defaultPoller(EventLoop *loop);
-
     virtual void update_channel(Channel *channel) = 0;
     virtual void remove_channel(Channel *channel) = 0;
-
+    
+    
+    //判断channel是否在当前poller中
+    bool has_channel(Channel *channel) const;
+    //获取这个事件循环的poller
+    static Poller *new_defaultPoller(EventLoop *loop);
 protected:
-    //key: fd  value:fd所属channel
+    //key: fd  value
+    // fd所属channel
     ChannelMap channels_;
-
 private:
     EventLoop *owner_loop_; //polllers所属事件循环
 };
