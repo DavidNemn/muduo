418da7124984a21e261171fd2ebd3cc3d76d6bbf
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-08 20:39:00 +0800

    Update InetAddress.hpp

--- MyMuduo/include/InetAddress.hpp
+++ MyMuduo/include/InetAddress.hpp
@@ -6,21 +6,18 @@
 
 using namespace std;
 
-//封装socket
+// 封装socket
 class InetAddress
 {
 public:
     explicit InetAddress(uint16_t port = 0, string ip = "127.0.0.1");
-
     explicit InetAddress(const sockaddr_in &addr) : addr_(addr) {}
 
     string get_ip() const;
     string get_ip_port() const;
     uint16_t get_port() const;
-
-    void set_sockaddr(const sockaddr_in &addr) { addr_ = addr; }
     const sockaddr_in *get_sockaddr() const { return &addr_; }
-
+    void set_sockaddr(const sockaddr_in &addr) { addr_ = addr; }
 private:
     sockaddr_in addr_;
 };
