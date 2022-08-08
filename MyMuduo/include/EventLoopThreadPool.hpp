36cca25749ef09ac51b1ea1392fb58c30147b990
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-08 18:51:10 +0800

    Update EventLoopThreadPool.hpp

--- MyMuduo/include/EventLoopThreadPool.hpp
+++ MyMuduo/include/EventLoopThreadPool.hpp
@@ -18,17 +18,15 @@ class EventLoopThreadPool : NonCopyable
 public:
     EventLoopThreadPool(EventLoop *baseloop, const string &name);
     ~EventLoopThreadPool();
-
-    //设定线程数量，一般应该和CPU核心数相同
-    void set_threadNum(int thread_num) { thread_nums_ = thread_num; }
-
     void start(const ThreadInitCallback &cb = ThreadInitCallback());
-
+    
     //如果工作在多线程，baseloop默认轮询方式分配channel给subloop
     EventLoop *get_nextEventLoop();
-
+    // 
     vector<EventLoop *> get_allLoops();
-
+    
+    // 设定线程数量，一般应该和CPU核心数相同
+    void set_threadNum(int thread_num) { thread_nums_ = thread_num; }
     bool get_started() const { return started_; }
     string get_name() const { return name_; }
 
@@ -39,6 +37,7 @@ private:
     bool started_;
     int thread_nums_;
     int next_;
-    vector<unique_ptr<EventLoopThread>> threads_; //所有线程，即所有subReactor
-    vector<EventLoop *> loops_;                   //每个线程里面对应的loop事件循环
+    // 
+    vector<unique_ptr<EventLoopThread>> threads_; // 所有线程，即所有subReactor
+    vector<EventLoop *> loops_;                   // 每个线程里面对应的loop事件循环
 };
