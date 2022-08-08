b951954909b4700eb93591d1d9753ee542efd947
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-08 21:21:09 +0800

    Update TcpConnection.hpp

--- MyMuduo/include/TcpConnection.hpp
+++ MyMuduo/include/TcpConnection.hpp
@@ -16,10 +16,11 @@ class Socket;
 using namespace std;
 
 /*
- *  tcpserver 通过accepter，有一个新用户连接，accep拿到connfd
- * tcpconnection设置channel相应回调，poller调用回调 
+ *  tcpserver 通过 accepter，有一个新用户连接，accep拿到connfd
+ *  tcpconnection设置channel相应回调，poller调用回调 
 */
-
+// 当一个类被共享智能指针 share_ptr 管理，且在类的成员函数里需要把当前类对象作为参数传给其他函数时
+// 这时就需要传递一个指向自身的 share_ptr。
 class TcpConnection : NonCopyable, public enable_shared_from_this<TcpConnection>
 {
 private:
@@ -35,38 +36,28 @@ public:
     TcpConnection(EventLoop *loop, const string name, int sockfd, const InetAddress &localaddr, const InetAddress &peeraddr);
     ~TcpConnection();
 
-    EventLoop *get_loop() const { return loop_; }
-
-    const string &get_name() const { return name_; }
-
-    const InetAddress &get_localaddr() const { return localaddr_; }
-
-    const InetAddress &get_peeraddr() const { return peeraddr_; }
-
-    bool connected() { return state_ == k_connected; }
-
-    void set_state(StateE state) { state_ = state; }
-
     //发送数据
     void send(const string& buf);
-
-    //断开连接
+    // 断开连接
     void shutdown();
-
-    //建立连接
+    // 建立连接
     void establish_connect();
-
-    //销毁连接
+    // 销毁连接
     void destory_connect();
 
-    //设置回调
+    // 设置回调
     void set_close_callback(const CloseCallback &callback) { close_callback_ = callback; }
     void set_highwater_callback(const HighWaterMarkCallback &callback) { highwater_callback_ = callback; }
-
     void set_connection_callback(const ConnectionCallback &callback) { connection_callback_ = callback; }
     void set_message_callback(const MessageCallback &callback) { message_callback_ = callback; }
     void set_write_complete_callback(const WriteCompleteCallback &callback) { write_complete_callback_ = callback; }
 
+    EventLoop *get_loop() const { return loop_; } 
+    const string &get_name() const { return name_; }
+    const InetAddress &get_localaddr() const { return localaddr_; }
+    const InetAddress &get_peeraddr() const { return peeraddr_; }
+    bool connected() { return state_ == k_connected; }
+    void set_state(StateE state) { state_ = state; }
 private:
     void handle_read(TimeStamp receive_time);
     void handle_write();
@@ -74,11 +65,10 @@ private:
     void handle_error();
 
     void send_inLoop(const string& buf);
-    //void shutdown();
+    // void shutdown();
     void shutdown_inLoop();
-
 private:
-    EventLoop *loop_; //所属subloop，非baseloop
+    EventLoop *loop_; // 所属subloop，非baseloop
     const string name_;
 
     atomic_int state_;
@@ -90,14 +80,13 @@ private:
     const InetAddress localaddr_;
     const InetAddress peeraddr_;
 
-    ConnectionCallback connection_callback_;        //有新连接时回调
-    MessageCallback message_callback_;              //有读写消息的回调
-    WriteCompleteCallback write_complete_callback_; //消息发送完以后的回调
+    ConnectionCallback connection_callback_;        // 有新连接时回调
+    MessageCallback message_callback_;              // 有读写消息的回调
+    WriteCompleteCallback write_complete_callback_; // 消息发送完以后的回调
     CloseCallback close_callback_;
-
     HighWaterMarkCallback highwater_callback_;
     size_t highwater_mark_; //高水位标志
 
-    Buffer input_buffer_;  //接收数据的缓冲区
-    Buffer output_buffer_; //发送数据的缓冲区
+    Buffer input_buffer_;  //  接收数据的缓冲区
+    Buffer output_buffer_; // 发送数据的缓冲区
 };
